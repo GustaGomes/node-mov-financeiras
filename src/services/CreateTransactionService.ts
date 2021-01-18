ce4fed1d221156d7ad469257822f8fd06d26ac31
Gustavo Gomes <gustavo.gomesspi@gmail.com>
2021-01-18 09:17:48 -0300

    projeto em node movimentações financeiras

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,26 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({title, value, type}: Request ) : Transaction {
+
+    if (!['income', 'outcome'].includes(type)){
+      throw new Error('Transaction type is invalid');
+    }
+    // aqui ele vai verificar se o tipo da transação é valida
+
+    const { total } = this.transactionsRepository.getBalance();
+
+    if( type == "outcome" && total < value ) {
+      throw new Error('Saldo insuficiente');
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
